ade5a6cb392fae77750b1b05b09f20a08f6ba7e8
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-08-04 20:29:28 +0530

    test2.c containing queries realated to extern keyword

--- design/test2.c
+++ design/test2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 extern int sharedValue;  // Declaration
+//sharedValue is defined in some other file maybe
 
 int main() {
     printf("%d\n", sharedValue);  // Works perfectly
